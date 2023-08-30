b5c13c26d7850a6da8a2ad26d18de22cc36496ec
Sama mohamed mahmoud abd ellatif <samamoh149d@gmail.com>
2023-08-31 00:38:08 +0300

    r

--- Recursion.py
+++ Recursion.py
@@ -1,18 +1,26 @@
-#using recrsion:
+    #using loops:
 def factorial(num):
-    if num == 1:
-        return 1
-    else:
-        return num * factorial(num - 1)
-    
-#using loops:
-def factorial2(num):
     i = num 
     fact = 1
     while i > 1:
         fact = fact * i
         i -= 1
-    return fact     
+    return fact    
+
+#using recrsion:
+def factorial2(num):
+    if num == 1:
+        return 1
+    else:
+        return num * factorial(num - 1) 
+
+#more efficient solution using tail-recursion:
+def factorial3(n, acc= 1):
+    if n == 0 :
+        return acc
+    return factorial3(n-1 , (n*acc))
+
 
 print (factorial(6)) 
 print (factorial2(6)) 
+print (factorial3(6)) 
